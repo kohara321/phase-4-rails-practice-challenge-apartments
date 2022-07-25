849294d3afe98e1c1d49fac30dda3633c17a61bc
Kaegan O'Hara <kohara321@gmail.com>
2022-07-25 11:47:40 -0600

    commit tree 8f611a9a27f8be5f59703f1b1499e5474f2884d4

---
+++ app/controllers/leases_controller.rb
@@ -0,0 +1,28 @@
+class LeasesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create
+        lease = Lease.create!(lease_params)
+        render json: lease, status: :created
+    end
+
+    def destroy
+        lease = find_lease
+        lease.destroy
+        head :no_content
+    end
+
+    private
+
+    def lease_params
+        params.permit(:rent, :apartment_id, :tenant_id)
+    end
+
+    def find_lease
+        Lease.find(params[:id])
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+end
